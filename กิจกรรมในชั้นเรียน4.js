fcfdecbc08789c19ee5d63277fb6f4abebd769ae
peemisoon <peemisoon@gmail.com>
2026-02-21 10:21:00 +0700

    Add files via upload

---
+++ กิจกรรมในชั้นเรียน4.js
@@ -0,0 +1,28 @@
+let n1 = 100;
+let n2 = 2.5;
+let s1 = "A";
+let s2 = "B";
+let s3 = "C5";
+let h = "Hello";
+
+console.log(n1);
+console.log(n2);
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log("--------------------");
+
+console.log(n1, n2);
+console.log(s1, s2);
+console.log(s3);
+console.log("--------------------");
+
+console.log(n1 + "" + n2, s1 + s2, s3);
+console.log("--------------------");
+
+console.log(n1 + h);
+console.log(n2 + h);
+console.log(h + s1);
+console.log("--------------------");
+
+console.log(n1 + h + n2 + h + s1);
